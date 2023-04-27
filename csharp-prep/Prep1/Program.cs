6511b2510d40e9c6f9a34a2fe6db72a0ff9a3673
Connor McIntyre <connormcintyre283.com@gmail.com>
2023-04-27 06:56:55 -0600

    Finished Prep 1 work

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine(first_name);
+
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine(last_name);
+
+        Console.WriteLine($"Your full name is: {first_name} {last_name}");
     }
 }
